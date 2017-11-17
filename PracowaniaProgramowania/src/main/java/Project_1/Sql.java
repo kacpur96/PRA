80633d1297739f7f6276f595ac6576538c83ce2f
kacpur96 <kacpur.amu@gmail.com>
2017-11-17 15:19:51 +0100

    Update Sql.java

--- PracowaniaProgramowania/src/main/java/Project_1/Sql.java
+++ PracowaniaProgramowania/src/main/java/Project_1/Sql.java
@@ -5,6 +5,7 @@ public class Sql {
 
     public boolean spr (String text) {
         check = "";
+        test = " ";
         test = (test.toUpperCase() + ' ');
         int t = text.length();
         int x = 0;
